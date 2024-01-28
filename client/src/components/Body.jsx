7e64ac7c37e6e14880d8ad6cd395bc24c2ee46ac
Michael <osujim2012@gmail.com>
2024-01-28 04:29:30 -0500

    separate leaderboard

--- client/src/components/Body.jsx
+++ client/src/components/Body.jsx
@@ -28,28 +28,7 @@ function speak(text) {
 
 // Body Component
 function Body() {
-  const data = [
-    { id: 1, name: "John", score: 5500 },
-    { id: 2, name: "Alice", score: 4530 },
-    { id: 3, name: "Bob", score: 4030 },
-    { id: 4, name: "Eva", score: 4002 },
-    { id: 5, name: "Charlie", score: 3807 },
-    { id: 6, name: "Sophia", score: 3700 },
-    { id: 7, name: "Daniel", score: 3211 },
-    { id: 8, name: "Olivia", score: 2020 },
-    { id: 9, name: "Liam", score: 1800 },
-    { id: 10, name: "Emma", score: 1000 },
-  ];
 
-  const generateRows = () => {
-    return data.map((item) => (
-      <div className="row p-2" key={`n${item.id}`}>
-        <div className="col-2">{item.id}</div>
-        <div className="col-7">{item.name}</div>
-        <div className="col-3">{item.score}</div>
-      </div>
-    ));
-  };
 
   const cardStyle = {
     width: "40rem",
@@ -61,21 +40,6 @@ function Body() {
     color: "white",
   };
 
-  const leaderboardStyle = {
-    width: "30rem",
-    height: "40rem",
-    backgroundColor: "#e7e5e53b",
-    borderColor: "rgb(90, 8, 8)",
-    borderWidth: "7px",
-    borderRadius: "60px",
-    color: "white",
-  }
-
-  const rankingStyle = {    
-    fontSize: "24px",
-
-
-  }
   const buttonStyle = {
     borderColor: "rgb(90, 8, 8)",
     borderWidth: "5px",
@@ -112,6 +76,7 @@ function Body() {
     BadFaith: "His goal is to “win” every argument rhetorically using any means necessary, he does not mind bending the facts.",
     GoodFaith: "His goal is to try his best to win the argument, using facts if they are available, but will admit if he is dead-wrong",
 };
+  const [showLeaderboard, setShowLeaderboard] = useState(false)
 
   const [currentModifier, setCurrentModifier] = useState(GaryModifiers.BadFaith)
 
@@ -326,14 +291,7 @@ function sendToChatGPT(text) {
 
         </div>
         <div className="col-3">
-      <div className="card" style={leaderboardStyle}>
-        <div className="card-body">
-          <h1 className="text-center pb-2">Leaderboards</h1>
-          <div className="container" style={rankingStyle}>
-            {generateRows()}
-          </div>
-        </div>
-      </div>
+    {showLeaderboard ? "" :<Leaderboard></Leaderboard>}
     </div>
       </div>      
       </div>
